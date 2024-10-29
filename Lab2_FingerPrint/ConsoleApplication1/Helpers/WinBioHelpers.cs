865f0098cf0b6c26ea05e00ef8f947ef4c9edde8
LukasNav <navasinskaslukas@gmail.com>
2024-10-29 16:31:32 +0200

    init lab2

---
+++ Lab2_FingerPrint/ConsoleApplication1/Helpers/WinBioHelpers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using WinBioWrapper.Types;
+
+namespace ConsoleApplication
+{
+    public class WinBioHelpers
+    {
+        // Functions to get window focus
+        // https://learn.microsoft.com/en-us/windows/win32/api/winbio/nf-winbio-winbioenrollcapture
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        static extern IntPtr GetConsoleWindow();
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+       
+        [DllImport("user32.dll")]
+        //[return: MarshalAs(IntPtr)]
+        static extern IntPtr SetActiveWindow(IntPtr hWnd);
+
+        public static void BringConsoleToFront()
+        {
+            SetForegroundWindow(GetConsoleWindow());
+        }
+
+        public static void SetActiveWindow()
+        {
+            SetActiveWindow(GetConsoleWindow());
+        }
+
+        // Check return Value and print error message if there's an error
+        public static bool CheckForErrVal(uint retVal)
+        {
+            if (retVal != 0)
+            {
+                Console.WriteLine("Error: " + ((WINBIO_ERRORS)retVal).ToString());
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("OK");
+                return false;
+            }
+        }
+
+        // Convert unmanaged pointer to an array of structs
+        public static void MarshalUnmananagedArray2Struct<T>(IntPtr unmanagedArray, int length, out T[] mangagedArray)
+        {
+            var size = Marshal.SizeOf(typeof(T));
+            mangagedArray = new T[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                IntPtr ins = new IntPtr(unmanagedArray.ToInt64() + i * size);
+                mangagedArray[i] = Marshal.PtrToStructure<T>(ins);
+            }
+        }
+    }
+}
---
+++ Lab2_FingerPrint/ConsoleApplication1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static List<Option> menuMain;
+        static void Main(string[] args)
+        {
+            // Create options that you want your menu to have
+            menuMain = new List<Option>
+            {
+                new Option("Print Internal Values", () => WinBioFuntions.PrintInternalValues()),
+                new Option("Enumerate Devices", () => WinBioFuntions.EnumerateDevices()),
+                new Option("Enumerate Databases", () => WinBioFuntions.EnumerateDatabases()),
+                new Option("Enumerate Enrollments", () => WinBioFuntions.EnumerateEnrollments()),
+                new Option("Open Session", () => WinBioFuntions.OpenSession()),
+                new Option("Locate Sensor", () => WinBioFuntions.LocateSensor()),
+                new Option("Select Biometric Subtype", () => WinBioFuntions.SelectBiometricSubtype()),
+                new Option("Identify", () => WinBioFuntions.Identify()),
+                new Option("Verify", () => WinBioFuntions.Verify()),
+                new Option("Enroll", () => WinBioFuntions.Enroll()),
+                new Option("Delete Template", () => WinBioFuntions.DeleteTemplate()),
+                new Option("Close Session", () => WinBioFuntions.CloseSession()),
+                new Option("Exit", () => Environment.Exit(0)),
+            };
+
+            // Set the default index of the selected item to be the first
+            int index = 0;
+
+            // Write the menu out
+            WriteMenu(menuMain, menuMain[index]);
+
+            // Store key info in here
+            ConsoleKeyInfo keyinfo;
+            do
+            {
+                keyinfo = Console.ReadKey();
+
+                // Handle each key input (down arrow will write the menu again with a different selected item)
+                if (keyinfo.Key == ConsoleKey.DownArrow)
+                {
+                    if (index + 1 < menuMain.Count)
+                    {
+                        index++;
+                        WriteMenu(menuMain, menuMain[index]);
+                    }
+                }
+                if (keyinfo.Key == ConsoleKey.UpArrow)
+                {
+                    if (index - 1 >= 0)
+                    {
+                        index--;
+                        WriteMenu(menuMain, menuMain[index]);
+                    }
+                }
+                // Handle different action for the option
+                if (keyinfo.Key == ConsoleKey.Enter)
+                {
+                    menuMain[index].Selected.Invoke();
+                    index = 0;
+                }
+            }
+            while (keyinfo.Key != ConsoleKey.X);
+
+            Console.ReadKey();
+
+        }
+
+        static void WriteMenu(List<Option> options, Option selectedOption)
+        {
+            Console.Clear();
+
+            foreach (Option option in options)
+            {
+                if (option == selectedOption)
+                {
+                    Console.Write("> ");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine(option.Name);
+            }
+        }
+    }
+}
